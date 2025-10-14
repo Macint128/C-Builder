17a33e65674655914c1a3b2bd122872f16862360
Macint128 <jim257302@gmail.com>
2025-10-14 17:56:45 +0900

    update launcher.c

--- C/launcher.c
+++ C/launcher.c
@@ -1,9 +1,1 @@
-#include <stdio.h>
-
-int main() {
-    char name[100];
-    printf("Enter your name: ");
-    scanf("%99s", name);
-    printf("Hello, %s! Welcome to your Mac build.\n", name);
-    return 0;
-}
+workflow_runs
