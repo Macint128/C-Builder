001052a2d08bc55645e34f0f37f9b98343f7c588
Macint128 <jim257302@gmail.com>
2025-10-14 21:23:48 +0900

    update launcher.c

--- C/launcher.m
+++ C/launcher.m
@@ -1,12 +1,23 @@
 #import <Cocoa/Cocoa.h>
 
+// 버튼 클릭 액션 처리용 클래스
+@interface ButtonHandler : NSObject
+- (void)sayHello:(id)sender;
+@end
+
+@implementation ButtonHandler
+- (void)sayHello:(id)sender {
+    NSAlert *alert = [[NSAlert alloc] init];
+    [alert setMessageText:@"Hello, 준호!"];
+    [alert runModal];
+}
+@end
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // 애플리케이션 객체 생성
         NSApplication *app = [NSApplication sharedApplication];
         
-        // 윈도우 생성
-        NSWindow *window = [[NSWindow alloc] initWithContentRect:NSMakeRect(0, 0, 300, 150)
+        NSWindow *window = [[NSWindow alloc] initWithContentRect:NSMakeRect(0,0,300,150)
                                                        styleMask:(NSWindowStyleMaskTitled |
                                                                   NSWindowStyleMaskClosable |
                                                                   NSWindowStyleMaskResizable)
@@ -15,12 +26,15 @@ int main(int argc, const char * argv[]) {
         [window setTitle:@"준호의 예제"];
         [window center];
 
+        // 버튼 처리 객체
+        ButtonHandler *handler = [[ButtonHandler alloc] init];
+
         // 버튼 1
         NSButton *button1 = [[NSButton alloc] initWithFrame:NSMakeRect(50, 50, 80, 30)];
         [button1 setTitle:@"안녕"];
         [button1 setButtonType:NSButtonTypeMomentaryPushIn];
         [button1 setBezelStyle:NSBezelStyleRounded];
-        [button1 setTarget:nil];
+        [button1 setTarget:handler];
         [button1 setAction:@selector(sayHello:)];
         [[window contentView] addSubview:button1];
 
@@ -32,15 +46,6 @@ int main(int argc, const char * argv[]) {
         [button2 setTarget:app];
         [button2 setAction:@selector(terminate:)];
         [[window contentView] addSubview:button2];
-
-        // 액션 구현 (Hello 출력)
-        id sayHelloBlock = ^(id sender){
-            NSAlert *alert = [[NSAlert alloc] init];
-            [alert setMessageText:@"Hello, 준호!"];
-            [alert runModal];
-        };
-        [button1 setTarget:[sayHelloBlock copy]];
-        [button1 setAction:@selector(invoke)];
 
         [window makeKeyAndOrderFront:nil];
         [app run];
