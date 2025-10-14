3e5c7ef1b5e700a011bed64336f8ecf9951a29e8
Macint128 <jim257302@gmail.com>
2025-10-15 07:49:37 +0900

    update launcher.c

--- C/launcher.m
+++ C/launcher.m
@@ -2,45 +2,51 @@
 
 @interface AppDelegate : NSObject <NSApplicationDelegate>
 @property (strong) NSWindow *window;
-@property (strong) NSButton *button;
+@property (strong) NSTextView *textView;
 @end
 
 @implementation AppDelegate
-- (void)applicationDidFinishLaunching:(NSNotification *)notification {
-    NSRect frame = NSMakeRect(0, 0, 300, 200);
-    self.window = [[NSWindow alloc] initWithContentRect:frame
-                                               styleMask:(NSWindowStyleMaskTitled |
-                                                          NSWindowStyleMaskClosable |
-                                                          NSWindowStyleMaskResizable)
-                                                 backing:NSBackingStoreBuffered
-                                                   defer:NO];
-    [self.window setTitle:@"Cocoa Demo"];
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // 윈도우 생성
+    self.window = [[NSWindow alloc]
+        initWithContentRect:NSMakeRect(0, 0, 400, 300)
+                  styleMask:(NSWindowStyleMaskTitled |
+                             NSWindowStyleMaskClosable |
+                             NSWindowStyleMaskResizable)
+                    backing:NSBackingStoreBuffered
+                      defer:NO];
+    [self.window setTitle:@"🍫 CocoaCraft"];
     [self.window center];
-
-    self.button = [[NSButton alloc] initWithFrame:NSMakeRect(100, 80, 100, 40)];
-    [self.button setTitle:@"Click me!"];
-    [self.button setTarget:self];
-    [self.button setAction:@selector(showMessage)];
-    [[self.window contentView] addSubview:self.button];
-
     [self.window makeKeyAndOrderFront:nil];
+    
+    // 텍스트 뷰
+    self.textView = [[NSTextView alloc] initWithFrame:self.window.contentView.bounds];
+    self.textView.string = @"🪄 코코아 레시피\n\n1. 따뜻한 우유 200ml\n2. 코코아 파우더 2스푼\n3. 설탕 약간\n4. 마시멜로 하나 던지기 🍬\n\n➡️ 저어주고 마시면 완성!";
+    self.textView.editable = NO;
+    [self.textView setFont:[NSFont systemFontOfSize:16]];
+    [self.window.contentView addSubview:self.textView];
+    
+    // 버튼
+    NSButton *drinkButton = [[NSButton alloc] initWithFrame:NSMakeRect(150, 20, 100, 40)];
+    [drinkButton setTitle:@"마시기 ☕️"];
+    [drinkButton setButtonType:NSButtonTypeMomentaryPushIn];
+    [drinkButton setBezelStyle:NSBezelStyleRounded];
+    [drinkButton setTarget:self];
+    [drinkButton setAction:@selector(drinkCocoa)];
+    [self.window.contentView addSubview:drinkButton];
 }
 
-- (void)showMessage {
-    NSAlert *alert = [[NSAlert alloc] init];
-    [alert setMessageText:@"Hello, macOS!"];
-    [alert runModal];
+- (void)drinkCocoa {
+    self.textView.string = @"(ゴクゴク...) 으으 따뜻해~ ☺️🍫";
 }
 
-- (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)sender {
-    return YES; // 창 닫으면 앱 종료
-}
 @end
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         NSApplication *app = [NSApplication sharedApplication];
-        AppDelegate *delegate = [[AppDelegate alloc] init];
+        AppDelegate *delegate = [AppDelegate new];
         [app setDelegate:delegate];
         [app run];
     }
