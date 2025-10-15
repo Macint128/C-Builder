665ec2a42ca2a6473bd1a5ba1a88b7b8c9023125
Macint128 <jim257302@gmail.com>
2025-10-15 13:27:52 +0900

    update launcher.c

--- C/launcher.m
+++ C/launcher.m
@@ -1,53 +1,33 @@
 #import <Cocoa/Cocoa.h>
 
-@interface AppDelegate : NSObject <NSApplicationDelegate>
-@property (strong) NSWindow *window;
-@property (strong) NSTextView *textView;
-@end
-
-@implementation AppDelegate
-
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // 윈도우 생성
-    self.window = [[NSWindow alloc]
-        initWithContentRect:NSMakeRect(0, 0, 400, 300)
-                  styleMask:(NSWindowStyleMaskTitled |
-                             NSWindowStyleMaskClosable |
-                             NSWindowStyleMaskResizable)
-                    backing:NSBackingStoreBuffered
-                      defer:NO];
-    [self.window setTitle:@"🍫 CocoaCraft"];
-    [self.window center];
-    [self.window makeKeyAndOrderFront:nil];
-    
-    // 텍스트 뷰
-    self.textView = [[NSTextView alloc] initWithFrame:self.window.contentView.bounds];
-    self.textView.string = @"🪄 코코아 레시피\n\n1. 따뜻한 우유 200ml\n2. 코코아 파우더 2스푼\n3. 설탕 약간\n4. 마시멜로 하나 던지기 🍬\n\n➡️ 저어주고 마시면 완성!";
-    self.textView.editable = NO;
-    [self.textView setFont:[NSFont systemFontOfSize:16]];
-    [self.window.contentView addSubview:self.textView];
-    
-    // 버튼
-    NSButton *drinkButton = [[NSButton alloc] initWithFrame:NSMakeRect(150, 20, 100, 40)];
-    [drinkButton setTitle:@"마시기 ☕️"];
-    [drinkButton setButtonType:NSButtonTypeMomentaryPushIn];
-    [drinkButton setBezelStyle:NSBezelStyleRounded];
-    [drinkButton setTarget:self];
-    [drinkButton setAction:@selector(drinkCocoa)];
-    [self.window.contentView addSubview:drinkButton];
-}
-
-- (void)drinkCocoa {
-    self.textView.string = @"(ゴクゴク...) 으으 따뜻해~ ☺️🍫";
-}
-
-@end
-
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         NSApplication *app = [NSApplication sharedApplication];
-        AppDelegate *delegate = [AppDelegate new];
-        [app setDelegate:delegate];
+
+        // 윈도우 생성
+        NSRect frame = NSMakeRect(100, 100, 400, 200);
+        NSWindow *window = [[NSWindow alloc]
+            initWithContentRect:frame
+                      styleMask:(NSWindowStyleMaskTitled |
+                                 NSWindowStyleMaskClosable |
+                                 NSWindowStyleMaskResizable)
+                        backing:NSBackingStoreBuffered
+                          defer:NO];
+        [window setTitle:@"☕️ Hello Cocoa!"];
+        [window makeKeyAndOrderFront:nil];
+
+        // 라벨 추가
+        NSTextField *label = [[NSTextField alloc] initWithFrame:NSMakeRect(130, 80, 140, 40)];
+        [label setStringValue:@"Hello, Cocoa!"];
+        [label setBezeled:NO];
+        [label setDrawsBackground:NO];
+        [label setEditable:NO];
+        [label setSelectable:NO];
+        [[window contentView] addSubview:label];
+
+        // 종료 단축키 Command + Q 동작 활성화
+        [NSApp setActivationPolicy:NSApplicationActivationPolicyRegular];
+        [NSApp activateIgnoringOtherApps:YES];
         [app run];
     }
     return 0;
